21392c22f08e9785b137ffc432e5db4cfe99423f
George Popovic <georgesmpopovic@gmail.com>
2021-02-01 19:24:01 -0500

    Edited AppServiceProvider to force HTTPS in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
+
+// Added these two namespaces so as to fix the closure issue. 
+// 01/28/2021
+
+use Illuminate\Support\Str;
+use Illuminate\Queue\SerializableClosure; 
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +32,24 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        Schema::defaultStringLength(191);
+
+        // Had to force HTTPS to function on Heroku and not have style disparities with HTTP. 
+        // 1/24/2021
+        if ($this->app->environment() == 'production') {
+            URL::forceScheme('https');
+        }
+
+    }
+
+    // Solution from 
+    // https://github.com/spatie/async/issues/52
+    protected function registerOpisSecurityKey()
+    {
+        if (Str::startsWith($key = $this->app['config']->get('app.key'), 'base64:')) {
+            $key = base64_decode(substr($key, 7));
+        }
+
+        SerializableClosure::setSecretKey($key);
     }
 }
